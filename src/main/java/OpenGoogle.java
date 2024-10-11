783ce45241b893d0011d9a53a93ee421f1e15106
Sassri24 <chamodganegoda@gmail.com>
2024-10-11 23:18:15 +0530

    add dependencies

--- src/main/java/OpenGoogle.java
+++ src/main/java/OpenGoogle.java
@@ -4,7 +4,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class OpenGoogle {
 
     public static void main(String[] arg){
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\Chamod Ganegoda\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver","C:\\Users\\Chamod Ganegoda\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+
+        //WebDriverManager.chromedriver().setup();
+
         WebDriver driver=new ChromeDriver();
+        driver.get("https://www.google.com");
     }
 }
