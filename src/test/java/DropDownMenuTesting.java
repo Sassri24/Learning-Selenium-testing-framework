7d77ff8187aaca3ad0c6aedff0e0100cd26bff51
Sassri24 <chamodganegoda@gmail.com>
2024-10-13 23:40:39 +0530

    start drop down menu testing

--- src/test/java/DropDownMenuTesting.java
+++ src/test/java/DropDownMenuTesting.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class DropDownMenuTesting {
 
@@ -11,5 +12,17 @@ public class DropDownMenuTesting {
         driver=new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.leafground.com/select.xhtml");
+    }
+
+    @Test
+    public void dropDownExample(){
+
+        //  1.1. ways of select values in Basic dropdown
+        //  1.2. get the number of dropdown options
+        //  1.3. using sendKeys select dropdown value
+        //  1.4. selecting value in a boostrap dropdown
+
+        // 2. Google search -pick a value from suggestions
+        // 3. Handle Hidden Auto Suggestions Drop down and search using DOM debugger trick
     }
 }
