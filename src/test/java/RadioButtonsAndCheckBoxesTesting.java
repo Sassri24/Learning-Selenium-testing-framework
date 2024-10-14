8da8b8c25a370f6af5a9f84dc24e8de09ca72674
Sassri24 <chamodganegoda@gmail.com>
2024-10-14 19:17:34 +0530

    finished radio button testing

--- src/test/java/RadioButtonsAndCheckBoxesTesting.java
+++ src/test/java/RadioButtonsAndCheckBoxesTesting.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -19,6 +21,37 @@ public class RadioButtonsAndCheckBoxesTesting {
         //get link
 
         driver.get("https://www.leafground.com/radio.xhtml");
+
+        //find the default select radio button
+
+        boolean chrome= driver.findElement(By.id("j_idt87:console2:0")).isSelected();
+        boolean firefox= driver.findElement(By.id("j_idt87:console2:1")).isSelected();
+        boolean safari= driver.findElement(By.id("j_idt87:console2:2")).isSelected();
+        boolean edge= driver.findElement(By.id("j_idt87:console2:3")).isSelected();
+
+        if(chrome){
+            String chromeText= driver.findElement(By.xpath("//label[@for='j_idt87:console2:0']")).getText();
+            System.out.println("default select radio button is : "+ chromeText);
+        } else if (firefox) {
+            String firefoxText= driver.findElement(By.xpath("//label[@for='j_idt87:console2:1']")).getText();
+            System.out.println("default select radio button is : "+ firefoxText);
+        } else if (safari) {
+            String safariText= driver.findElement(By.xpath("//label[@for='j_idt87:console2:2']")).getText();
+            System.out.println("default select radio button is : "+ safariText);
+        } else if (edge) {
+            String edgeText= driver.findElement(By.xpath("//label[@for='j_idt87:console2:3']")).getText();
+            System.out.println("default select radio button is : "+ edgeText);
+        }
+
+        //Select the age group (only if not selected)
+
+        WebElement myAgeGroup=driver.findElement(By.id("j_idt87:age:0"));
+        boolean isChecked = myAgeGroup.isSelected();
+        if (!isChecked){
+            driver.findElement(By.xpath("//label[@for='j_idt87:age:0']")).click();
+        }
+
+
 
     }
 }
