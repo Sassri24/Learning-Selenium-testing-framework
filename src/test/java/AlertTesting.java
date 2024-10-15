b91e7cb414b600a97a453ac05308f7fb9ef91b5b
Sassri24 <chamodganegoda@gmail.com>
2024-10-15 16:51:19 +0530

    finished alert testing

--- src/test/java/AlertTesting.java
+++ src/test/java/AlertTesting.java
@@ -1,4 +1,7 @@
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -16,6 +19,30 @@ public class AlertTesting {
 
     @Test
     public void alertExample(){
+
+        //Alert (Simple Dialog)
+
+        WebElement dialogBox=driver.findElement(By.id("j_idt88:j_idt91"));
+        dialogBox.click();
+        Alert alert=driver.switchTo().alert();
+        alert.accept();
+
+        //Alert (Confirm Dialog)- cancel alert massage
+
+        WebElement cancelDialog=driver.findElement(By.id("j_idt88:j_idt93"));
+        cancelDialog.click();
+        Alert alert2= driver.switchTo().alert();
+        alert2.dismiss();
+
+        //Alert (Prompt Dialog)
+
+        driver.findElement(By.id("j_idt88:j_idt104")).click();
+        Alert alert3= driver.switchTo().alert();
+        String dialogBoxText= alert3.getText();
+        System.out.println(dialogBoxText);
+        alert3.sendKeys("Chamod");
+        alert3.accept();
+
 
     }
 }
