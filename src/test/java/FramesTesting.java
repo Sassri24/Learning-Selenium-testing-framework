797afad4c189b532378cf7f2c95d81052e55046f
Sassri24 <chamodganegoda@gmail.com>
2024-10-16 16:00:55 +0530

    finished frames testing

--- src/test/java/FramesTesting.java
+++ src/test/java/FramesTesting.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class FramesTesting {
 
     WebDriver driver;
@@ -27,10 +29,23 @@ public class FramesTesting {
         button1.click();
 
         String afterClickButtonText= button1.getText();
-        System.out.println("after click inside frame button text "+ afterClickButtonText);
+        System.out.println("after click inside frame button text: "+ afterClickButtonText);
 
 
         // 2. Click mr inside nested frame
+
+        driver.switchTo().defaultContent(); // switch to home page
+        driver.switchTo().frame(2); // switch to nested frame
+        driver.switchTo().frame(0); // switch to child frame
+
+        WebElement button3 = driver.findElement(By.id("Click"));
+        button3.click();
+        System.out.println("after click nested button frame text: "+button3.getText());
+
         // 3. How many frames in this page
+
+        driver.switchTo().defaultContent();
+        List<WebElement> iframes= driver.findElements(By.tagName("iframe"));
+        System.out.println(iframes.size());
     }
 }
