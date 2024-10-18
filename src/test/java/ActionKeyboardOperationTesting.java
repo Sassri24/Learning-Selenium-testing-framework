0d32a1f1a2594cadbe7d1e931af26631904de813
Sassri24 <chamodganegoda@gmail.com>
2024-10-18 23:14:18 +0530

    add before Method

--- src/test/java/ActionKeyboardOperationTesting.java
+++ src/test/java/ActionKeyboardOperationTesting.java
@@ -1,3 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
 public class ActionKeyboardOperationTesting {
 
+    WebDriver driver;
+
+    @BeforeMethod
+    public void openGoogle(){
+
+    }
 }
