40649b81d5fef9c1198ccc067e4f26809fcb24c5
Sassri24 <chamodganegoda@gmail.com>
2024-10-18 23:15:55 +0530

    Open chrome driver

--- src/test/java/ActionKeyboardOperationTesting.java
+++ src/test/java/ActionKeyboardOperationTesting.java
@@ -1,7 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 
 public class ActionKeyboardOperationTesting {
 
