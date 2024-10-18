ed6c1440bc74479997b415fe14479d235af5c043
Sassri24 <chamodganegoda@gmail.com>
2024-10-18 23:17:24 +0530

    remove some packages

--- src/test/java/ActionMouseOperationTesting.java
+++ src/test/java/ActionMouseOperationTesting.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.interactions.SourceType;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
