1c4b4dbe8142c65d4b50cb3749c3c355a5c7567a
Sassri24 <chamodganegoda@gmail.com>
2024-10-22 16:41:22 +0530

    start download testing

--- src/test/java/FilesDownloadUploadTesting.java
+++ src/test/java/FilesDownloadUploadTesting.java
@@ -1,6 +1,9 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class FilesDownloadUploadTesting {
 
@@ -13,5 +16,14 @@ public class FilesDownloadUploadTesting {
         driver=new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.leafground.com/file.xhtml");
+    }
+
+    @Test
+    public void fileDownloadTest() throws InterruptedException {
+        WebElement downloadButton = driver.findElement(By.id("j_idt93:j_idt95"));
+        downloadButton.click();
+
+        Thread.sleep(4000);
+
     }
 }
