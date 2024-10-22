eaf5383701a8f2f012ffe76c8eac73bc8382ada4
Sassri24 <chamodganegoda@gmail.com>
2024-10-22 16:32:30 +0530

    start download testing

--- src/test/java/FilesDownloadUploadTesting.java
+++ src/test/java/FilesDownloadUploadTesting.java
@@ -1,6 +1,17 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
 
 public class FilesDownloadUploadTesting {
 
     WebDriver driver;
+
+    @BeforeMethod
+
+    public void openFileTestPage(){
+
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.leafground.com/file.xhtml");
+    }
 }
