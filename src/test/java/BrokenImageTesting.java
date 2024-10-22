de3106e84c72e59b8535c21d1c50bdc382b88555
Sassri24 <chamodganegoda@gmail.com>
2024-10-22 23:05:16 +0530

    start broken image testing

---
+++ src/test/java/BrokenImageTesting.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+
+public class BrokenImageTesting {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void openBrowser(){
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("");
+    }
+}
