9fd21c6d4938cae23b8042ded7765b77763255cc
Sassri24 <chamodganegoda@gmail.com>
2024-10-24 20:36:36 +0530

    broken image testing with naturalWidth

--- src/test/java/BrokenImageTesting.java
+++ src/test/java/BrokenImageTesting.java
@@ -1,6 +1,11 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class BrokenImageTesting {
 
@@ -12,8 +17,19 @@ public class BrokenImageTesting {
     public void openWebSite(){
         driver=new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("");
+        driver.get("https://the-internet.herokuapp.com/broken_images");
     }
 
     //broken Image example 01
+
+    @Test
+    public void brokenImageExample(){
+        List<WebElement> brokenImages= driver.findElements(By.xpath("//div[@class='example']/img"));
+
+        for(WebElement image : brokenImages){
+            if(image.getAttribute("naturalWidth").equals(0)){
+                System.out.println();
+            }
+        }
+    }
 }
