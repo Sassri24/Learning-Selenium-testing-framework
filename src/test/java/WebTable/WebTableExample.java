cc1179e3707cb873482a5e4943a5ce71a7f75a49
Sassri24 <chamodganegoda@gmail.com>
2024-12-13 19:20:39 +0530

    table column and row count testing

--- src/test/java/WebTable/WebTableExample.java
+++ src/test/java/WebTable/WebTableExample.java
@@ -1,6 +1,36 @@
 package WebTable;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
 public class WebTableExample {
 
-    //new
+    WebDriver driver;
+
+    @BeforeMethod
+    public void openChrome(){
+        driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://testautomationpractice.blogspot.com/");
+    }
+
+    @Test
+    public void tableTesting(){
+
+        //how many rows in the table
+
+        int tableRowCount= driver.findElements(By.xpath("//table[@id='productTable']/tbody/tr")).size();
+        System.out.println("tableRowCount: "+tableRowCount);
+
+        //how many columns in the table
+
+        int tableColumnCount= driver.findElements(By.xpath("//table[@id='productTable']/thead/tr/th")).size();
+        System.out.println("table column count: " + tableColumnCount);
+    }
 }
